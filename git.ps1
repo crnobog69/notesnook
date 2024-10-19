6a16342bb0d37b4a5afef943f79f6827c3d3d6d6
crnobog <carskalavra@proton.me>
2024-10-19 13:53:12 +0200

    ❄️

---
+++ git.ps1
@@ -0,0 +1,8 @@
+echo "# notesnook" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "❄️"
+git branch -M main
+git remote add origin git@github.com:crnobog69/notesnook.git
+git push -u origin main
